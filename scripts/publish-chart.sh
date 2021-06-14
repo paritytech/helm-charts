f55eadfd1ce9f351c58965984b24faff048b88f9
Pierre Besson <pierre.besson@parity.io>
2021-06-14 17:30:15 +0200

    add script to publish chart to GCS

---
+++ scripts/publish-chart.sh
@@ -0,0 +1,12 @@
+# Package helm chart archive
+helm package .
+mkdir charts
+mv node*.tgz charts
+# Generate updated index based on the existing one
+gsutil cp gs://parity-helm-charts/index.yaml index.yaml
+helm repo index \
+  --url https://parity-helm-charts.storage.googleapis.com \
+  --merge index.yaml \
+  charts/
+# Copy new archive and updated index to bucket
+gsutil cp -r charts/* gs://parity-helm-charts
