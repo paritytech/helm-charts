f5f151aa04751582793bfaddd45212392e07e5f2
Pierre Besson <pierre.besson@parity.io>
2023-07-26 13:43:58 +0200

    remove polkadot-runtime-exporter chart

--- chart-testing/run-local.sh
+++ chart-testing/run-local.sh
@@ -5,6 +5,5 @@ targetBranch="$(git branch --show-current)"
 ct lint-and-install \
   --target-branch="${targetBranch}" \
   --charts="${changed}" \
-  --excluded-charts=charts/polkadot-runtime-exporter \
   --validate-chart-schema=false \
   --lint-conf="chart-testing/lintconf.yaml"
