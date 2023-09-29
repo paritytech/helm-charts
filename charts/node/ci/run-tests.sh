18a2b30d892da6ea2c8a51187d794cca0d0d185e
bakhtin <a@bakhtin.net>
2023-09-29 10:44:12 +0100

    Shorten service name
    Signed-off-by: bakhtin <a@bakhtin.net>

--- charts/node/ci/run-tests.sh
+++ charts/node/ci/run-tests.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-KUSTOMIZE_APPLY_ATTEMPTS=3
+KUSTOMIZE_APPLY_ATTEMPTS=5
 until kustomize build --enable-helm addons | kubectl apply -f -; do
   echo "Retrying to install addons"
   if [[ $((KUSTOMIZE_APPLY_ATTEMPTS--)) -eq 0 ]]; then
     echo "Failed to install addons"
     exit 1
   fi
-  sleep 5
+  sleep 10
 done
 
 kustomize build --enable-helm . | kubectl apply -f -
